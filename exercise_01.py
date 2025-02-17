4afea398aa6b26b18c41bc64cb9f6b0e8e30d851
Balagbag01 <andreiegualan1@gmail.com>
2025-02-17 18:27:28 +0800

    Update exercise_01.py

--- exercise_01.py
+++ exercise_01.py
@@ -6,15 +6,10 @@
 count = 10  # 'count' is assigned 10
 Count = 15  # 'Count' (different from 'count') is assigned 15
 total_count = 20  # Another integer literal assignment
-
 decimal_value = 3.14  # 'decimal_value' is assigned 3.14
-
 message = "Hello, Python!"  # Assigning a text value
-
 is_active = True  # Boolean value representing True
-
 result = None  # Represents absence of value
-
 print("Integer (count):", count)
 print("Integer (Count):", Count)
 print("Integer (total_count):", total_count)
